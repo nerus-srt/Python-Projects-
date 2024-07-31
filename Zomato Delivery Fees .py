e7d91726574cc62ae835cc4890395c4a10f6dcb8
nerus-srt <nerus.srt@outlook.com>
2024-07-31 20:28:35 +0530

    first commit

---
+++ Zomato Delivery Fees .py
@@ -0,0 +1,20 @@
+#Programmer: Suren Raj Tualdhar
+#Date:18/07/2022
+#Program: Food Delivery Fees
+
+name = str(input("Enter the name"))
+address = str(input("Enter your Delivery Address"))
+amount =int(input("Enter Total Payable Amount"))
+if(amount >100):
+    print("Congratulation")
+    print("You are food will be delivered in 30min")
+    print("Enjoy our free Delivery Service")
+    print("Bon Appetite ")
+    print("For More Contact Us @ +971 524707211 ")
+else:
+    print("Congratulation")
+    print("You are food will be delivered in 30min")
+    print("To Enjoy our free Delivery Service")
+    print("Minumun Purchasse Order AED 100")
+    print("Bon Appetite ")
+    print("For More Contact Us @ +971 524707211")
