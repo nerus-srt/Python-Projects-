bdd74d9208c0174d2f09be7b8ec6543c5b9abd0f
Suren R. Tuladhar <128699986+nerus-srt@users.noreply.github.com>
2024-08-02 21:05:43 +0530

    Add files via upload

---
+++ VOX Cinema Ticket System.py
@@ -0,0 +1,11 @@
+#Programmer: Suren Raj Tualdhar
+#Date:13/07/2022
+#Program: VOX Cinema Ticket System
+
+name = str(input("Enter the name"))
+age = int(input("Enter your age"))
+if(age <13):
+    print(" You belong to children category")
+else:
+    print("You are belong senior Category")
+    print("Thank you for using our ticketing system")
